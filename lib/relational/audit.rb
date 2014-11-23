56629f966bc300941ecfca26fb3c906f36b0c69a
unknown <ksmanoj@KSMANOJ-T420.jnpr.net>
2014-11-24 01:55:43 +0530

    first commit

---
+++ lib/relational/audit.rb
@@ -0,0 +1,7 @@
+require "relational/audit/version"
+
+module Relational
+  module Audit
+    # Your code goes here...
+  end
+end
---
+++ lib/relational/audit/version.rb
@@ -0,0 +1,5 @@
+module Relational
+  module Audit
+    VERSION = "0.0.1"
+  end
+end
